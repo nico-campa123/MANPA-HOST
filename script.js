232792d1e589943115dde735e87668f2e1eff36c
nico-campa123 <48675751@est.ort.edu.ar>
2025-10-04 16:38:38 -0400

    god return

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Replace with your real backend URL
-const API_URL = "https://python-test-deploy1.onrender.com";
+const API_URL = "https://plebeyoo.onrender.com";
 
 async function checkAPI() {
   try {
@@ -19,3 +19,29 @@ async function checkAPI() {
 }
 
 checkAPI();
+
+document.getElementById("uploadBtn").addEventListener("click", async () => {
+  const fileInput = document.getElementById("csvFile");
+  if (!fileInput.files.length) {
+    console.log("No file selected");
+    return;
+  }
+
+  const file = fileInput.files[0];
+  const formData = new FormData();
+  formData.append("file", file);
+
+  try {
+    const res = await fetch(`${API_URL}/upload-csv/`, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!res.ok) throw new Error(`Server error: ${res.status}`);
+
+    const data = await res.json();
+    console.log("Number of characters in CSV:", data.characters);
+  } catch (err) {
+    console.error("Error uploading file:", err);
+  }
+});
